0a6c75b364b36f06a3d49980439bdaa3d2027d04
Solaris <91217295+RohanVolety@users.noreply.github.com>
2023-03-23 22:10:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,31 @@
-#import the flask module
-
 from flask import Flask, render_template, request
-
+import requests
+import time
 
 app = Flask(__name__)
 
-#make a route and render all the html templates in this route
-
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        city_name = request.form.get('city')
+def hello():
+    if request.method == "POST":
+        city = request.form['city']
+        api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=ENTER YOUR API KEY HERE"
+        response = requests.get(api)
+        data = response.json()
+        temp = round(data['main']['temp'] - 273.15)
+        pressure = data['main']['pressure']
+        humidity = data['main']['humidity']
+        sunrise = time.strftime('%I:%M', time.gmtime(
+            data['sys']['sunrise'] - 21600))
+        sunset = time.strftime('%I:%M', time.gmtime(
+            data['sys']['sunset'] - 21600))
 
-        #take a variable to show the json data
-       
-        json_object = {"coord": {"lon": 77.2167, "lat": 28.6667}, "weather": [{"id": 701, "main": "Mist", "description": "mist", "icon": "50d"}], "base": "stations", "main": {"temp": 285.2, "feels_like": 284.73, "temp_min": 285.2, "temp_max": 285.2, "pressure": 1018, "humidity": 87}, "visibility": 1200, "wind": {
-            "speed": 2.57, "deg": 270}, "clouds": {"all": 1}, "dt": 1672055440, "sys": {"type": 1, "id": 9165, "country": "IN", "sunrise": 1672018912, "sunset": 1672056059}, "timezone": 19800, "id": 1273294, "name": "Delhi", "cod": 200}
-       
-        #read the json object
-        #json_object = r.json()
+        return render_template('home.html', city=city ,temp=temp, pressure=pressure, humidity=humidity, sunrise=sunrise, sunset=sunset)
 
-        #take some attributes like temperature,humidity,pressure of this
-        # this temparetuure in kelvin
-        temperature = int(json_object['main']['temp']-273.15)
-        humidity = int(json_object['main']['humidity'])
-        pressure = int(json_object['main']['pressure'])
-        wind = int(json_object['wind']['speed'])
-
-        #atlast just pass the variables
-        condition = json_object['weather'][0]['main']
-        desc = json_object['weather'][0]['description']
-
-        return render_template('home.html', temperature=temperature, pressure=pressure, humidity=humidity, city_name=city_name, condition=condition, wind=wind, desc=desc)
-    else:
-        return render_template('home.html')
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
+
+
